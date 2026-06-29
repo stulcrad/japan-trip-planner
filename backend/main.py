ec4572fcf0ecc599715701847d1b86cac57295c0
Radek Stulc <stulcrad@gmail.com>
2026-06-29 20:42:36 +0200

    Only allow vercel frontend

--- backend/main.py
+++ backend/main.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://japan-trip-planner-ruddy.vercel.app/"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
